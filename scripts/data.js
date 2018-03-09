6ae590220e82a729ebbb71ae7e9888afd9e0e874
Bryan Kai <brykai123@gmail.com>
2018-03-09 14:06:03 -0800

    Working Progress Bar

---
+++ scripts/data.js
@@ -0,0 +1,16 @@
+let recipes = {
+  'beef': {
+    'steak':  {
+      'rare': {'temp': '128 degrees','time': '1 hour'},
+      'medium': {'temp': '128 degrees','time': '1 hour'}
+    },
+    'tri-tip': {
+      'rare': {'temp': '128 degrees','time': '1 hour'},
+      'medium': {'temp': '128 degrees','time': '1 hour'}
+    }
+  },
+  'chicken': {}
+}
+
+
+let titleArr = ['MEAT TYPE', 'CUT TYPE', 'DONENESS'];
--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,49 @@
-console.log("I'm linked to the HTML file.")
+//// CONNECTION TESTING /////
+// Linked to HTML?
+// console.log("I'm linked to the HTML file.")
+
+// // Data.js linked?
+// console.log(recipes.beef.steak.rare)
+// var data = require('./data.js');
+
+
 //Initialize variables.
-const button = document.querySelectorAll('.button')
+const button = document.querySelectorAll('.buttons-sub-container>.button')
+const progBarStatus = document.querySelectorAll('.progress-bar>.progress')
+const clearButton = document.querySelector('.clear-button')
+let progBarArr = []
+
 
 //Event listeners
-for (let i=0; i<button.length; i++) {
+for (let i = 0; i < button.length; i++) {
   button[i].addEventListener('click', render)
 }
+clearButton.addEventListener('click', emptyProgBar)
 
-
-
-
-// Progress bar
-
-function render() {
-  console.log('Click!')
+///// FUNCTIONS /////
+// Progress bar //
+function render(event) {
+  console.log(event.target.innerHTML + ' clicked!')
+  updateProgBar(event)
 }
 
-function createProgBar() {
-  let progBar = []
-  progBar.push(button.innerHTML)
-  console.log
+function updateProgBar(event) {
+  progBarArr.push(event.target.innerHTML)
+  for (let i = 0; i < progBarStatus.length; i++) {
+    if (progBarArr[i]) {
+      progBarStatus[i].classList.add("complete")
+    }
+  }
+}
+
+function emptyProgBar() {
+  progBarArr = []
+  for (let i = 0; i < progBarStatus.length; i++) {
+    progBarStatus[i].classList.remove("complete")
+  }
+}
+
+// Buttons //
+function populateButtons() {
+  
 }
