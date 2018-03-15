b002d4382a4d31bdd0bf9813c78b3a3bd8dbd376
Bryan Kai <brykai123@gmail.com>
2018-03-15 10:33:40 -0700

    Added chicken, pork, seafood recipe data

--- scripts/data.js
+++ scripts/data.js
@@ -1,24 +1,124 @@
 let recipes = {
   'Beef': {
-    'Steak':  {
-      'Rare': {'temp': '125','time': '1 hour'},
-      'Medium-rare': {'temp': '128','time': '1 hour'},
-      'Medium': {'temp': '135','time': '1 hour'},      'Medium-well': {'temp': '145','time': '1 hour'},
+    'Steak': {
+      'Rare': {
+        'temp': '125',
+        'time': '1 hour'
+      },
+      'Medium Rare': {
+        'temp': '128',
+        'time': '1 hour'
+      },
+      'Medium': {
+        'temp': '135',
+        'time': '1 hour'
+      },
+      'Medium-well': {
+        'temp': '145',
+        'time': '1 hour'
+      },
     },
-    'Tri-tip': {
-      'Rare': {'temp': '130','time': '2 hours'}
+    'Tri-tip/ Brisket': {
+      'Rare': {
+        'temp': '131',
+        'time': '24 hours'
+      },
+      'Medium Rare': {
+        'temp': '149',
+        'time': '12 - 24 hours'
+      },
+      'Well Done': {
+        'temp': '185',
+        'time': '8 - 16 hours'
+      },
     }
   },
   'Pork': {
     'Pork Chops': {
-      'Rare': {'temp': '130','time': '1 to 4 hours'},
-      'Medium-rare': {'temp': '140','time': '1 to 4 hours'},
-      'Medium-well': {'temp': '150','time': '1 to 4 hours'},
-      'Well-done': {'temp': '160','time': '1 to 4 hours'}
+      'Rare': {
+        'temp': '135',
+        'time': '1 - 3 hours'
+      },
+      'Medium Rare': {
+        'temp': '140',
+        'time': '1 - 3 hours'
+      },
+      'Medium Well': {
+        'temp': '150',
+        'time': '1 - 3 hours'
+      },
+      'Well Done': {
+        'temp': '160',
+        'time': '1 - 3 hours'
+      }
+    },
+    'Pork Belly': {
+      'Rare': {
+        'temp': '140',
+        'time': '8 - 24 hours'
+      },
+      'Medium Rare': {
+        'temp': '154',
+        'time': '8 - 24 hours'
+      },
+      'Well Done': {
+        'temp': '185',
+        'time': '8 - 16 hours'
+      }
     }
   },
-  'Chicken': {},
-  'Seafood': {}
+  'Chicken': {
+    'Light Meat': {
+      'Soft and Juicy': {
+        'temp': '140',
+        'time': '1 - 3 hours'
+      },
+      'Tender and Juicy': {
+        'temp': '149',
+        'time': '1 - 3 hours'
+      },
+      'Juicy but firm': {
+        'temp': '160',
+        'time': '1 - 3 hours'
+      }
+    },
+    'Dark Meat': {
+      'Tender': {
+        'temp': '140',
+        'time': '45 min - 5 hours'
+      },
+      'Falling Off the Bone': {
+        'temp': '167',
+        'time': '45 min - 5 hours'
+      }
+    }
+  },
+  'Seafood': {
+    'Salmon': {
+      'Tender': {
+        'temp': '104',
+        'time': '30 min - 1 hour'
+      },
+      'Tender and Flaky': {
+        'temp': '122',
+        'time': '30 min - 1 hour'
+      },
+      'Well Done': {
+        'temp': '131',
+        'time': '30 min - 1 hour'
+      }
+    },
+    'Lobster': {
+      'Tender': {
+        'temp': '130',
+        'time': '1 hour'
+      },
+      'Firm': {
+        'temp': '140',
+        'time': '1 hour'
+      }
+    }
+  }
 }
 
 
